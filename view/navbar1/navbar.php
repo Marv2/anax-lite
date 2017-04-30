c2d79b2eb13b5da735b0fb9d2518dc7b6577d591
Marv2 <maria.s.karlsson@gmail.com>
2017-04-30 11:46:10 +0100

    Navbar1

---
+++ view/navbar1/navbar.php
@@ -0,0 +1,38 @@
+<?php
+
+$navbar = [
+
+    "config" => [
+        "navbar-class" => "navbar"
+    ],
+    "items" => [
+        "start" => [
+            "text" => "Start",
+            "route" => "",
+        ],
+        "about" => [
+            "text" => "About",
+            "route" => "about",
+        ],
+        "report" => [
+            "text" => "Report",
+            "route" => "report",
+        ],
+    ]
+];
+
+$navHtml = "";
+$navClass = $navbar["config"]["navbar-class"];
+
+// Create url using $app->url->create();
+foreach ($navbar["items"] as $items => $item) {
+    $nav = $item["route"];
+    $url = $app->url->create("$nav");
+
+    $navHtml .= '<li><a href="' . $url . '">' . $item["text"] . '</a></li>';
+}
+?>
+
+<nav class="<?= $navClass ?>">
+    <?= $navHtml ?>
+</nav>
